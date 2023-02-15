4f74d93bbed977105913a4adc418971fde9e54b0
Jalen Moses <116610117+jalenmo509@users.noreply.github.com>
2023-02-15 09:12:07 -0800

    Add files via upload

---
+++ singlyLinkedList/src/com/jalen/singlyLinkedList/Node.java
@@ -0,0 +1,13 @@
+package com.jalen.singlyLinkedList;
+
+public class Node {
+    // MEMBER VARIABLES
+	public int value;
+    public Node next;
+    
+    // CONSTRUCTOR
+    public Node(int value) {
+        this.value = value;
+        next = null;
+    }
+}
