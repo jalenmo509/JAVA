0004d52d9c1946012660bc5db9f24a42198d3eab
Jalen Moses <116610117+jalenmo509@users.noreply.github.com>
2023-02-15 11:00:40 -0800

    Add files via upload

---
+++ CodingDojo/burgertracker/src/main/java/com/jalen/burgertracker/BurgertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jalen.burgertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgertrackerApplication.class, args);
+	}
+
+}
