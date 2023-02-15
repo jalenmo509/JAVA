002ee150232d5f89a144bae521a8c3dff55a0563
Jalen Moses <116610117+jalenmo509@users.noreply.github.com>
2023-02-15 12:21:27 -0800

    Add files via upload

---
+++ FullSpring/LoginUser/src/main/java/com/jalen/authentication/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jalen.authentication.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jalen.authentication.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+	List<User> findAll();
+}
